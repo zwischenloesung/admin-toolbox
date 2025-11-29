6e25348b3c070d65dbaa7f066a7bbc7c22890425
Michael Lustenberger <mic@inofix.ch>
2025-11-30 00:35:31 +0100

    Display Names
    Add proper display names on every level.

--- maestro-basin-source-gen.py
+++ maestro-basin-source-gen.py
@@ -40,7 +40,7 @@ def parse_stdin(do_override_uuids=False):
         print("Not implemented in this context yet.")
     lines = [l.rstrip() for l in sys.stdin if l.strip()]
     if not lines:
-        return None, None, None, None, []
+        return None, None, None, None, None, None, []
     combined = slugify(lines[0])
     index = lines[1].strip() if len(lines) > 1 else "0000"
     subs = []
@@ -52,14 +52,21 @@ def parse_stdin(do_override_uuids=False):
         unit = parts[1].strip() if len(parts) > 1 else ""
         stype = parts[2].strip() if len(parts) > 2 else "float"
         subs.append((subname, unit, stype))
-    return combined, index, None, None, subs
+    return combined, index, None, None, None, None, subs
 
 def parse_interactive(do_override_uuids):
 
     combined_name = slugify(input("Enter CombinedSensor type name (empty to finish): ").strip())
     if not combined_name:
-        return None, None, None, None, []
-    index = input("Enter the CombinedSensor index ('-' to skip, [0000]): ").strip() or "0000"
+        return None, None, None, None, None, None, []
+    index = input("Enter the source index ('-' to skip, [0000]): ").strip() or "0000"
+    type_displname = input(
+        "Enter type display name (empty to skip all display names): "
+    ).strip()
+    if type_displname:
+        i = f"{type_displname} {index}" if index else type_displname
+        tmp = input(f"Enter source display name ([{i}]): ").strip()
+        source_displname = tmp if tmp else i
     if do_override_uuids:
         combined_type_uuid = input(
             "Enter sourcetype UUID (empty means autogenerate): "
@@ -80,14 +87,23 @@ def parse_interactive(do_override_uuids):
         else:
             sub_type_uuid = None
             sub_source_uuid = None
-        displ_name = input("Enter display name (empty to skip): ").strip()
+        if type_displname:
+            sub_type_displname = input(
+                f"Enter sourcetype display name ([{sub_in}] if empty): "
+            ).strip()
+            sub_type_displname = sub_type_displname if sub_type_displname else sub_in
+            i = f"{sub_type_displname} {index}" if index else type_displname
+            sub_source_displname = input(
+                f"Enter source display name ([{i}]): "
+            ).strip()
+            sub_source_displname = sub_source_displname if sub_source_displname else i
         dev_type = input("Enter device-type (empty means autogenerate): ").strip()
         u = guess_unit(sub_name)
         unit = input(f"Enter unit for '{sub_name}' [{u}]: ").strip()
         if not unit:
             unit = u
         stype = input(f"Enter type for '{sub_name}' (default float): ").strip() or "float"
-        meta = parse_meta()
+        sub_meta = parse_meta()
 
         skipit = slugify(input("Please confirm the entry ([Y/n]): ").strip())
         if not skipit.lower() == "n":
@@ -95,13 +111,22 @@ def parse_interactive(do_override_uuids):
                 sub_name,
                 sub_type_uuid,
                 sub_source_uuid,
-                displ_name,
+                sub_type_displname,
+                sub_source_displname,
                 dev_type,
                 unit,
                 stype,
-                meta
+                sub_meta
             ))
-    return combined_name, index, combined_type_uuid, combined_source_uuid, sub_sensors
+    return (
+        combined_name,
+        index,
+        combined_type_uuid,
+        combined_source_uuid,
+        type_displname,
+        source_displname,
+        sub_sensors
+    )
 
 # Quoted string wrapper
 class Quoted(str): pass
@@ -142,6 +167,10 @@ def guess_unit(name):
 
 # limitting depth is not really needed, but might be interesting later for non-interactive
 def quote_textlike(obj, depth=None):
+    # stop and return None if obj is empty
+    if not obj:
+        return None
+
     # stop descending if depth is a non-None or negative
     if depth is not None and depth < 0:
         return obj
@@ -172,6 +201,8 @@ def produce_output(
         index,
         combined_type_uuid,
         combined_source_uuid,
+        type_displname,
+        source_displname,
         sub_sensors,
         do_prepend_parent=False
     ):
@@ -184,59 +215,73 @@ def produce_output(
     src_combined_uuid = combined_source_uuid if combined_source_uuid else gen_uuid()
 
     indexs = "" if index == "-" else f"-{index}"
-    sources.append({
+    tmp = {
         "uuid": q(src_combined_uuid),
         "name": q(f"{combined_name}{indexs}"),
         "parentname": q(""),
         "parentuuid": q(""),
         "typeuuid": q(st_combined_uuid),
-    })
-    sourcetypes.append({
+    }
+    if source_displname:
+        tmp["meta"] = { "displayname": { "en": quote_textlike(source_displname) } }
+    sources.append(tmp)
+    tmp = {
         "uuid": q(st_combined_uuid),
         "name": q(combined_name),
         "class": q("CombinedSensor"),
         "devicetype": q(combined_name),
         "type": None,
         "unit": None,
-    })
+    }
+    if type_displname:
+        tmp["meta"] = { "displayname": { "en": quote_textlike(type_displname) } }
+    sourcetypes.append(tmp)
 
     # Sub-sensors
     for (
         sub_name,
         sub_type_uuid,
         sub_uuid,
-        displ_name,
+        sub_type_displname,
+        sub_source_displname,
         dev_type,
         unit,
         stype,
-        meta
+        sub_source_meta
     ) in sub_sensors:
         st_uuid = sub_type_uuid if sub_type_uuid else gen_uuid()
         src_uuid = sub_uuid if sub_uuid else gen_uuid()
         src_name = f"{combined_name}{indexs}-{sub_name}" if do_prepend_parent else sub_name
-        if displ_name:
+        sub_type_meta = {}
+
+        if sub_type_displname:
             # TODO only "en" currently supported and no way to turn the input question off
-            meta["displayname"] = { "en": displ_name }
+            sub_source_meta["displayname"] = { "en": sub_source_displname }
+            sub_type_meta["displayname"] = { "en": sub_type_displname }
         dt = dev_type if dev_type else f"{combined_name}-{sub_name}"
 
-        meta = quote_textlike(meta) #TODO: in non-interacte we might want to limit this..!?
-
-        sources.append({
+        #TODO: in non-interacte we might want to limit the depth of quote_textlike()..!?
+        tmp = {
             "uuid": q(src_uuid),
             "name": q(src_name),
             "parentname": q(f"{combined_name}{indexs}"),
             "parentuuid": q(src_combined_uuid),
             "typeuuid": q(st_uuid),
-            "meta": meta,
-        })
-        sourcetypes.append({
+        }
+        if sub_source_meta:
+            tmp["meta"] = quote_textlike(sub_source_meta),
+        sources.append(tmp)
+        tmp = {
             "uuid": q(st_uuid),
             "name": q(f"{combined_name}-{sub_name}"),
             "class": q("Sensor"),
             "devicetype": q(dt),
             "type": stype,  # bare literal
             "unit": q(unit),
-        })
+        }
+        if sub_type_meta:
+            tmp["meta"] = quote_textlike(sub_type_meta)
+        sourcetypes.append(tmp)
         mapping[sub_name] = src_uuid
 
     data = {"sources": sources, "sourcetypes": sourcetypes}
@@ -277,11 +322,21 @@ def main():
 
     do_continue = True
     while do_continue:
-        combined_name, index, tuuid, suuid, sub_sensors = parse(do_override_uuids)
+        combined_name, index, tuuid, suuid, tdname, sdname, sub_sensors = parse(
+            do_override_uuids
+        )
         if not combined_name:
             do_continue = False
         else:
-            sensor_libs.append([ combined_name, index, tuuid, suuid, sub_sensors ])
+            sensor_libs.append([
+                combined_name,
+                index,
+                tuuid,
+                suuid,
+                tdname,
+                sdname,
+                sub_sensors
+            ])
     print(sensor_libs)
 
     for s in sensor_libs:
@@ -289,8 +344,17 @@ def main():
         index = s[1]
         tuuid = s[2]
         suuid = s[3]
-        sub_sensors = s[4]
-        produce_output(combined_name, index, tuuid, suuid, sub_sensors, do_prepend_parent)
+        tdname = s[4]
+        sdname = s[5]
+        sub_sensors = s[6]
+        produce_output(
+            combined_name,
+            index,
+            tuuid,
+            suuid,
+            tdname,
+            sdname,
+            sub_sensors, do_prepend_parent)
 
 
 
